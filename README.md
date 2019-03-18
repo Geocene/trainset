f230ff6776d60bd9d7604649de3010f67ce6c278
Rush Kapoor <rush.kapoor@berkeley.edu>
2019-03-17 21:25:12 -0700

    fixed routing issue in build

--- README.md
+++ README.md
@@ -14,6 +14,9 @@ npm run dev
 # build for production with minification
 npm run build
 
+# testing script for serving prod build locally
+npm run start
+
 # build for production and view the bundle analyzer report
 npm run build --report
 ```
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
+const history = require('connect-history-api-fallback');
 
 let app = express();
+app.use(history());
 app.use(serveStatic(__dirname + "/dist"));
 
 const port = process.env.PORT || 5000;
