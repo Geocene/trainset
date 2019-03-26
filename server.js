175c22f20bb5c3fcdc044d834ad9591840ff4351
Rush Kapoor <rush.kapoor@berkeley.edu>
2019-03-26 14:22:11 -0700

    modified https forwarding

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.use(history());
 app.use(serveStatic(__dirname + "/dist"));
 
 const port = process.env.PORT || 5000;
+
+app.use((req, res, next) => {
+  if (req.header('x-forwarded-proto') !== 'https') {
+	res.redirect(`https://${req.header('host')}${req.url}`);
+  } else {
+    next();
+  }
+});
+
 app.listen(port, () => {
   console.log('Listening on port ' + port)
 });
