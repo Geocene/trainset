02f0675c684be3c6a903d3ad2c2eec186fa72839
Rush Kapoor <rush.kapoor@berkeley.edu>
2019-03-26 14:27:22 -0700

    fix https forwarding fix #51

--- server.js
+++ server.js
@@ -2,20 +2,15 @@ const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
 const history = require('connect-history-api-fallback');
+const secure = require('ssl-express-www');
 
 let app = express();
+
+app.use(secure);
 app.use(history());
 app.use(serveStatic(__dirname + "/dist"));
 
 const port = process.env.PORT || 5000;
-
-app.use((req, res, next) => {
-  if (req.header('x-forwarded-proto') !== 'https') {
-	res.redirect(`https://${req.header('host')}${req.url}`);
-  } else {
-    next();
-  }
-});
 
 app.listen(port, () => {
   console.log('Listening on port ' + port)
