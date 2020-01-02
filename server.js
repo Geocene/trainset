67166a80d82dc90a25ad128f80ac9b5f5e813dc8
Konrad Jurk <konrad.jurk@gmail.com>
2020-01-03 00:28:08 +0100

    Remove SSL

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
 const history = require('connect-history-api-fallback');
-const secure = require('ssl-express-www');
 
 let app = express();
 
-app.use(secure);
 app.use(history());
 app.use(serveStatic(__dirname + "/dist"));
 
